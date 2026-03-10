5ebef786d67aca15e56c2dfac4dd5d13679d75f8
Malonee58 <miraclemalonee58@gmail.com>
2026-03-10 07:31:48 +0100

    Fix blank page caused by HashRouter basename

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import Technologies from "./Pages/Technologies";
 
 function App() {
 	return (
-		<Router basename="/miracle-portfolio">
+		<Router>
 			<Header />
 			<Routes>
-				<Route path="/Home" element={<Home />} />
+				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/technologies" element={<Technologies />} />
 			</Routes>
 			<Footer />
-		</Router> 
+		</Router>
 	);
 }
 
